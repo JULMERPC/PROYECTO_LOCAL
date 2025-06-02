e43ea21485f945cce713b43e21a3a3e454c1d886
Julmer Puma <julmerpumacondori@gmail.com>
2025-06-01 20:02:48 -0500

    actualizado

--- ms-proveedor - copia/src/main/java/com/example/ms/proveedor/exception/DuplicateEmailException.java
+++
@@ -1,7 +0,0 @@
-package com.example.ms.proveedor.exception;
-
-public class DuplicateEmailException extends RuntimeException {
-    public DuplicateEmailException(String message) {
-        super(message);
-    }
-}
